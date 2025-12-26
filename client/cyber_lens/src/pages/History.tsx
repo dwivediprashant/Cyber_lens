3bbdbc8b05cb5d3893abb6a0e1d090f4b2215560
LooninS <hushed55@proton.me>
2025-12-27 00:57:10 +0530

    Issue 2: configured routing and base layout(in App.tsx),created pages,navbar,footer

---
+++ client/cyber_lens/src/pages/History.tsx
@@ -0,0 +1,11 @@
+const History = () => {
+    return (
+        <div>
+            <h2>History</h2>
+            <p>This is the History page.</p>
+        </div>
+
+    );
+};
+
+export default History;
---
+++ client/cyber_lens/src/pages/News.tsx
@@ -0,0 +1,10 @@
+const News = () => {
+    return (
+        <div className="page-container">
+            <h2>News</h2>
+            <p>This is the News page.</p>
+        </div>
+    );
+};
+
+export default News;
